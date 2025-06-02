e495a5ef0de8390c01f3927953e8e021873ec9ae
CCXLV <abracadabragio12@gmail.com>
2025-06-02 19:48:09 +0400

    Add redis

--- app/views/index/views.py
+++ app/views/index/views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from redis import Redis
 
 index_views = Blueprint("index_views", __name__, url_prefix="/")
-
+redis_client = Redis(host="redis", port=6379)
 
 @index_views.route("/")
 def index():
-    return render_template("index.html")
+    visit_amount = redis_client.incrby(f"ip:{request.remote_addr}", 1)
+
+    return render_template("index.html", visit_amount=visit_amount)
